e664e7f04a5e11d8100d270bcffb83681dcb66cd
adtejaa <dharmatejapallapu2002@gmail.com>
2025-05-31 01:10:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,9 +6,10 @@ terraform {
     }
   }
 }
-provider "microsoft365wp" {
-  client_id     = "pleae provide client_id"
-  client_secret = "please provide client_secret"
-  tenant_id     = "tenant_id"
-  use_oidc = true
-}
+
+# provider "microsoft365wp" {
+#   client_id     = "pleae provide client_id"
+#   client_secret = "please provide client_secret"
+#   tenant_id     = "tenant_id"
+#   use_oidc = true
+# }
