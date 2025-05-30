f0e039297a9f31119fd810a85a2fbc70699aef51
adtejaa <dharmatejapallapu2002@gmail.com>
2025-05-31 01:46:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,6 +7,18 @@ terraform {
   }
 }
 
+
+terraform { 
+  cloud { 
+    
+    organization = "intune-adtejaa" 
+
+    workspaces { 
+      name = "intune-dev" 
+    } 
+  } 
+}
+
 # provider "microsoft365wp" {
 #   client_id     = "pleae provide client_id"
 #   client_secret = "please provide client_secret"
