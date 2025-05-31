04476af879bf62830f1d836f756e1a4341a59f3a
adtejaa <dharmatejapallapu2002@gmail.com>
2025-05-31 23:49:14 +0530

    Update windows_configuration.tf

--- windows_configuration.tf
+++ windows_configuration.tf
@@ -5,3 +5,27 @@ resource "microsoft365wp_device_configuration" "windows10_general" {
     screen_capture_blocked = true
   }
 }
+
+
+resource "microsoft365wp_notification_message_template" "test" {
+  display_name = "TF Test 1"
+
+  localized_notification_messages = [
+    {
+      locale           = "en-us"
+      subject          = "Test en-us"
+      message_template = "This is a test message."
+      is_default       = true
+    },
+    {
+      locale           = "de-de"
+      subject          = "Test de-de"
+      message_template = "Dies ist eine Testnachricht."
+    },
+    {
+      locale           = "es-es"
+      subject          = "Test es-es"
+      message_template = "Este es un mensaje de prueba."
+    }
+  ]
+}
